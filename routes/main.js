14796a8e9d4d7ff23e9772c4665bbb5fdec72452
mpirbhoy <mujtaba.pirbhoy@mail.utoronto.ca>
2015-11-20 20:07:19 -0500

    Main and def.img

--- routes/main.js
+++ routes/main.js
@@ -23,6 +23,24 @@ module.exports = function(app, passport) {
 		res.render('./pages/register');
 	});
 
+	//POST REQUESTS
+	//
+	//Request to perform login
+	app.post('/login', passport.authenticate('local-login', { successRedirect: '/profile',
+														failureRedirect: '/login', failureFlash : true}));
+
+	//Request to perform signup
+	app.post('/signup', passport.authenticate('local-signup', {
+			successRedirect : '/profile', // redirect to the secure profile section
+			failureRedirect : '/signup'// redirect back to the signup page if there is an error
+	, failureFlash : true}));
+
+	//Request to perform logout
+	app.post('/logout', function (req, res) {
+		req.logout();
+		res.redirect('/');
+	});
+
 }
 
 //Middleware for passing through if person is logged in already
