611d63e822446e85f5325db2b8e99332ac2505e2
mpirbhoy <mujtaba.pirbhoy@mail.utoronto.ca>
2015-11-22 15:27:06 -0500

    removed unncessary middleware

--- controller/middleware.js
+++ controller/middleware.js
@@ -10,7 +10,7 @@ module.exports.isLoggedIn = function isLoggedIn(req, res, next) {
 }
 
 //debugging logs
-function logger(req, res, next) {
+module.exports = function logger(req, res, next) {
 	//console.log(req.body.username);
 	//console.log(req.body.password);
 	return next();
@@ -25,35 +25,4 @@ module.exports.isNotLoggedIn = function isNotLoggedIn(req, res, next) {
 
 	// if they are redirect them to the profile page
 	res.redirect('/main');
-}
-
-
-//Generates randomname of length length while avoiding duplicates 
-function generateName(length) {
-	var rString = randomString(length);
-
-	function imgExists(name) {
-		fs.exists("./img/" + name, function(exists) {
-			return exists;
-		});
-	}
-
-	function randomString(length) {
-		var result = '';
-		var chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-		for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
-		return result;
-	}
-
-	while (imgExists(rString)) {
-		rString = randomString(length);
-	}
-
-	return rString;
-}
-
-//Helper function to hashEmail
-function hashEmail(email) {
-	var hash = crypto.createHash('md5').update(email).digest('hex');
-	return hash;
 }
