880f890d2b612d40ebb4b21fc7cbe81bd3209da1
Jung Ho Son <jungsgames@gmail.com>
2015-12-04 16:45:36 -0500

    finished delete functionality

--- franks_a4/app.js
+++ franks_a4/app.js
@@ -16,7 +16,7 @@ var app = express();
 
 
 // connect to mongoose
-mongoose.connect('mongodb://localhost:27017/test');
+mongoose.connect('mongodb://localhost:27017/tests');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function callback() {
