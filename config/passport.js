db663c2c5ee4a81ea51962782b543a0f2892d379
mpirbhoy <mujtaba.pirbhoy@mail.utoronto.ca>
2015-12-05 18:39:18 -0500

    facebook first user is admin now

--- config/passport.js
+++ config/passport.js
@@ -166,6 +166,7 @@ module.exports = function (passport) {
                                     // if successful, return the new user
                                     return done(null, newUser);
                                 });
+                            });
 
                         });
                     }
